cbafe193c943b4f584fb1426e2396c324f65e774
Hridasov Sviatoslav <svatoslavgridasov5@gmail.com>
2026-09-22 20:28:59 +0300

    зміни 2

--- Project2.1/Project2.1.cpp
+++ Project2.1/Project2.1.cpp
@@ -11,13 +11,14 @@ int main()
 {
 	double a; //вхідне значення
 	double z1; //результат обчислення першого виразу
-	//double z2; //результат обчислення 2 виразу
+	double z2; //результат обчислення 2 виразу
 	cout << "a = "; cin >> a;
 	z1 = 1 - 0.25 * (sin(2*a) * sin(2*a)) + cos(2*a);
-	//z2 = cos(a) * cos(a) + cos(a) * cos(a) * cos(a) * cos(a);
+	z2 = cos(a) * cos(a) + cos(a) * cos(a) * cos(a) * cos(a);
 	cout << endl;
 	cout << "z1 = " << z1 << endl; // вивід результату z1
-	//cout << "z2 = " << z2 << endl; // вивід результату z2
+	cout << "z2 = " << z2 << endl; // вивід результату z2
 	cin.get();
 	return 0;
 }
+// текст для зміни 2 
