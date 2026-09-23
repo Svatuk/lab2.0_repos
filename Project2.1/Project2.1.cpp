65c239e2ebdc58e7003d767d4a0566947032f752
Hridasov Sviatoslav <svatoslavgridasov5@gmail.com>
2026-09-23 10:25:37 +0300

    перший коміт у новій гілці

--- Project2.1/Project2.1.cpp
+++ Project2.1/Project2.1.cpp
@@ -14,7 +14,7 @@ int main()
 	double z2; //результат обчислення 2 виразу
 	cout << "a = "; cin >> a;
 	z1 = 1 - 0.25 * (sin(2*a) * sin(2*a)) + cos(2*a);
-	z2 = cos(a) * cos(a) + cos(a) * cos(a) * cos(a) * cos(a);
+	z2 = cos(a) * cos(a) + cos(a) * cos(a) * cos(a) * cos(a);//нова гілка а це зміни для коміту в ній 
 	cout << endl;
 	cout << "z1 = " << z1 << endl; // вивід результату z1
 	cout << "z2 = " << z2 << endl; // вивід результату z2
